d664309147c0284dff3cc10e004660a38fc201a5
Harun Yiğit <harun00yigit@gmail.com>
2021-04-30 19:11:16 +0300

    jrb
    jrb kütüphanesi eklendi ve okunan kilit elemanları ağaca atılıp ekrana yazdırma testleri yapıldı.
    
    str testleri için kullanılan diziler kaldırıldı.

--- test/src/main.c
+++ test/src/main.c
@@ -1,47 +1,46 @@
-/* Use the fields library to print each word on standard input, labeled with its line number. */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "fields.h"
+#include "jrb.h"
 //#include <stdbool.h> //bool gerekirse
 
 int main(int argc, char **argv)
 {
+	JRB b;
+	JRB bn;
     IS file; // girdi dosyası (input struct)
-  	char *words[500];    //kelimelerin tutulduğu dizi
- 	int wordFreq[500] = {0}; //kelimelerin tekrar değerlerinin tutulduğu dizi
-	int sayac = 0; 	     //kelime sayacı
+  	char* kelime; 		//kelimelerin tırnaklardan ayrılmış hâli(words'e düzgün atama için)
+	char* kod;			//kodların tırnaklardan ayrılmış hâli(words'e düzgün atama için)
+	
     if (argc != 2) { fprintf(stderr, "usage: printwords filename\n"); exit(1); 
 }
  
     /* Open the file as an inputstruct.  Error check. */
 
     file = new_inputstruct(argv[1]);
+    b = make_jrb();
+    
     if (file == NULL) {
     	perror(argv[1]);
     	exit(1);
   	}
 	
   	while(get_line(file) >= 0) {
-    	for (int i = 0; i < file->NF; i++) {
-    		words[sayac] = strdup(file->fields[i]); //strdup duplicate
-      		for(int j = 0; sayac >= 1 && j < sayac; j++){     		
-      			if(strcmp(words[sayac], words[j]) == 0){  //strcmp compare eşitse 0 döndürür
-      				wordFreq[j] += 1;
-      				sayac -= 1;
-      			}   		
-      		}
-			sayac += 1;
-    	}
-    }
-    
-	printf("\nj-kelime-freq\n");
-	
-	for (int j = 0; j < sayac; j++) {
-		printf("%d=>%s:%d\n", j, words[j], wordFreq[j]);
- 	}
+    	
+    		if(file->NF != 1){
+    			kelime = strtok(file->fields[file->NF-2], "\""); //strtok tırnaklardan ayırıyor
+				kod = strtok(file->fields[file->NF-1], "\""); 
 
+    			(void) jrb_insert_str(b, strdup(kelime), new_jval_s(strdup(kod)));
+    		}
+      	
+    }
+	
+	jrb_traverse(bn,b) {
+    	printf("%s\t%s\n", bn->key.s, bn->val.s);	
+  	}
+	
   	jettison_inputstruct(file);
   	return 0;
 }
